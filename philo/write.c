1f1b7a7f8801487d1cfdd7ccdb9a68481a3122d4
Beatriz Fiochi Soares Da Silva <bfiochi-@student.42.fr>
2025-08-10 13:09:01 +0100

    fix the tab before write philo_status and update the gitignore

--- philo/write.c
+++ philo/write.c
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 17:32:05 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/08/08 18:00:56 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/08/10 13:04:15 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void	write_philo_status(t_philo_status status, t_philo *philo)
 		&& (!simulation_finished(philo->table)))
 		printf("%-6ld %d has taken a fork\n", elapsed, philo->id);
 	else if (status == EATING && !simulation_finished(philo->table))
-		printf("%6ld %d is eating\n", elapsed, philo->id);
+		printf("%-6ld %d is eating\n", elapsed, philo->id);
 	else if (status == SLEEPING && !simulation_finished(philo->table))
-		printf("%6ld %d is sleeping\n", elapsed, philo->id);
+		printf("%-6ld %d is sleeping\n", elapsed, philo->id);
 	else if (status == THINKING && !simulation_finished(philo->table))
-		printf("%6ld %d is thinking\n", elapsed, philo->id);
+		printf("%-6ld %d is thinking\n", elapsed, philo->id);
 	else if (status == DIED)
-		printf("%6ld %d died\n", elapsed, philo->id);
+		printf("%-6ld %d died\n", elapsed, philo->id);
 	handle_mutex(&philo->table->write_mutex, UNLOCK);
 }
